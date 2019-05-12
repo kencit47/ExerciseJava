057a137b73b893383884af027d25d59cc9ef5d90
kencit47 <kencit47@gmail.com>
2019-05-12 21:12:06 +0700

    + Update test case 2

--- src/test/java/Pratice_UCLNTest.java
+++ src/test/java/Pratice_UCLNTest.java
@@ -6,8 +6,9 @@ public class Pratice_UCLNTest {
     @Test
     public void TestFind_UCLN(){
         assertEquals(new Practice_UCLN().Find_UCLN(12,168),12);
-        assertEquals(new Practice_UCLN().Find_UCLN(0,0),0);
+        assertEquals(new Practice_UCLN().Find_UCLN(0,20),0);
         assertEquals(new Practice_UCLN().Find_UCLN(168,12),12);
+        assertEquals(new Practice_UCLN().Find_UCLN(12,12),12);
         assertEquals(new Practice_UCLN().Find_UCLN(169,12),1);
     }
 

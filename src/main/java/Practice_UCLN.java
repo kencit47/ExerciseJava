b70a312e9eceb8162290035158632224c28e314d
kencit47 <kencit47@gmail.com>
2019-05-12 20:55:42 +0700

    + Update dieu kien 2

--- src/main/java/Practice_UCLN.java
+++ src/main/java/Practice_UCLN.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 public class Practice_UCLN {
 
     public int Find_UCLN(int a, int b){
+        if(a == 0 || b == 0)
+            return 0;
         if(a == b)
             return a;
         if(a % b == 0) {

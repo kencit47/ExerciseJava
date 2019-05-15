efd45a8fbe2e695d441d3ce24cfcbdfa84f10e2b
kencit47 <kencit47@gmail.com>
2019-05-15 21:46:49 +0700

    + Remove if(n ==0) return 0; + Update test case

--- src/main/java/GiaiThua.java
+++ src/main/java/GiaiThua.java
@@ -2,8 +2,6 @@ public class GiaiThua {
 
     public long tinhGiaiThua(int n){
         long result = 1;
-        if(n == 0)
-            return 0;
         for(int i = 1; i <= n; i++){
             result *= i;
         }

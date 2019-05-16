68b6966dbc1b8c89cade21e4adf623eb38c7260e
kencit47 <kencit47@gmail.com>
2019-05-16 07:36:05 +0700

    + Update while -> do...while

--- src/main/java/TinhPi_2.java
+++ src/main/java/TinhPi_2.java
@@ -4,13 +4,15 @@ public class TinhPi_2 {
         int mauso = 2;
         double pi = 3.0;
         double tuso = 4.0;
-        double esp = tuso/(mauso * (mauso + 1) * (mauso + 2)) - tuso/(((mauso + 2) * (mauso + 3) * (mauso + 4)));
+        double esp;
 
-        while (esp > saiso){
+        do
+        {
+            esp = tuso/(mauso * (mauso + 1) * (mauso + 2)) - tuso/(((mauso + 2) * (mauso + 3) * (mauso + 4)));
             pi += esp;
             mauso += 4;
-            esp = tuso/(mauso * (mauso + 1) * (mauso + 2)) - tuso/(((mauso + 2) * (mauso + 3) * (mauso + 4)));
-        }
-        return pi + saiso;
+
+        }while (esp > saiso);
+        return pi;
     }
 }

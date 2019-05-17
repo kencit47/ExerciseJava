1a9adf1185def463865ed7f536726d15d8ee84dc
kencit47 <kencit47@gmail.com>
2019-05-17 19:40:36 +0700

    + Optimaze code : remove ham tinh giai thua

--- src/main/java/TinhE.java
+++ src/main/java/TinhE.java
@@ -4,24 +4,14 @@ public class TinhE {
         final double saiso = 0.00001;
         int i = 0;
         double e = 0.0;
-        double esp;
+        double esp = 1.0;
 
         do{
-            esp = (double) 1 / giaiThua(i);
             e += esp;
             i++;
+            esp *= (double) 1 / i;
         }while(esp > saiso);
 
         return e;
-    }
-
-    private long giaiThua(int n){
-        long result = 1;
-
-        for(int i = 1; i <= n; i++){
-            result *= i;
-        }
-
-        return result;
     }
 }

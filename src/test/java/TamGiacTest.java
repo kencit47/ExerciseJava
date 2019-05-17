3ee8ab5c261ca5d090e982cf837bd0fb218ab567
kencit47 <kencit47@gmail.com>
2019-05-17 23:03:03 +0700

    + Update optimaze 4

--- src/test/java/TamGiacTest.java
+++ src/test/java/TamGiacTest.java
@@ -9,7 +9,9 @@ public class TamGiacTest {
         assertEquals(new TamGiac().tamGiac(4,3,9),"Khong phai tam giac");
         assertEquals(new TamGiac().tamGiac(9,4,3),"Khong phai tam giac");
         assertEquals(new TamGiac().tamGiac(4,9,3),"Khong phai tam giac");
-        assertEquals(new TamGiac().tamGiac(9,9,9),"Type - chu vi - dien tich : Tam giac can - 27 - 35.074028853269766");
+        assertEquals(new TamGiac().tamGiac(9,8,9),"Type - chu vi - dien tich : Tam giac can - 26 - 32.2490309931942");
+        assertEquals(new TamGiac().tamGiac(9,9,8),"Type - chu vi - dien tich : Tam giac can - 26 - 32.2490309931942");
+        assertEquals(new TamGiac().tamGiac(8,9,9),"Type - chu vi - dien tich : Tam giac can - 26 - 32.2490309931942");
         assertEquals(new TamGiac().tamGiac(3,4,5),"Type - chu vi - dien tich : Tam giac vuong - 12 - 6.0");
         assertEquals(new TamGiac().tamGiac(5,4,3),"Type - chu vi - dien tich : Tam giac vuong - 12 - 6.0");
         assertEquals(new TamGiac().tamGiac(3,5,4),"Type - chu vi - dien tich : Tam giac vuong - 12 - 6.0");

bd73eb0c9090db8b2d9192b637ec70a499541e0e
kencit47 <kencit47@gmail.com>
2019-05-21 20:38:36 +0700

    + Add practice 25 : fibonaci m<= x <= n

---
+++ src/test/java/Fibonaci25Test.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Fibonaci25Test {
+    @Test
+    public void test_fibonaci25(){
+        assertEquals(new Fibonaci25().fibonaci25(0,2),4);
+        assertEquals(new Fibonaci25().fibonaci25(0,0),1);
+        assertEquals(new Fibonaci25().fibonaci25(2,50),7);
+    }
+}

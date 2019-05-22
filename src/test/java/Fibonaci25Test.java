4013f74e4f5f9b73c9685fedf2a2ed53dbe1cb12
kencit47 <kencit47@gmail.com>
2019-05-22 21:20:47 +0700

    + Update test case

--- src/test/java/Fibonaci25Test.java
+++ src/test/java/Fibonaci25Test.java
@@ -7,6 +7,7 @@ public class Fibonaci25Test {
     public void test_fibonaci25(){
         assertEquals(new Fibonaci25().fibonaci25(0,2),4);
         assertEquals(new Fibonaci25().fibonaci25(0,0),1);
+        assertEquals(new Fibonaci25().fibonaci25(1,5),5);
         assertEquals(new Fibonaci25().fibonaci25(2,50),7);
     }
 }

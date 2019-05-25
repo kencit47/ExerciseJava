e431ab864342d7045d681484fcd01a3c7325c0c1
kencit47 <kencit47@gmail.com>
2019-05-25 20:57:28 +0700

    + Optimaze code

--- src/main/java/thapPhanSangLama.java
+++ src/main/java/thapPhanSangLama.java
@@ -5,15 +5,14 @@ public class thapPhanSangLama {
         int[] arr = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
         String[] arrLM = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
 
-        while (a > 0){
-            for(int i = 0; i < arr.length; i++){
+            for(int i = 0; true; i++){
                 if(a - arr[i] >= 0){
                     a -= arr[i];
                     result += arrLM[i];
-                    break;
+                    i = 0;
                 }
-            }
+                if(a == 0)
+                    return result;
         }
-        return result;
     }
 }

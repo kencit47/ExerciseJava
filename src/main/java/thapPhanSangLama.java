ad2d0e938acf7dd0d32f9c0b8d0a5de5ea9240d4
kencit47 <kencit47@gmail.com>
2019-05-25 19:02:16 +0700

    + Add practice 30 : chuyen thap phan sang lama

---
+++ src/main/java/thapPhanSangLama.java
@@ -0,0 +1,19 @@
+public class thapPhanSangLama {
+
+    public String thapPhanSangLaMa(int a){
+        String result = "";
+        int[] arr = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] arrLM = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        while (a > 0){
+            for(int i = 0; i < arr.length; i++){
+                if(a - arr[i] >= 0){
+                    a -= arr[i];
+                    result += arrLM[i];
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}

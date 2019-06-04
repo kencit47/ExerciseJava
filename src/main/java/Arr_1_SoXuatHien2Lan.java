55a547313781120f7ef7d336bd147ef51810b291
DESKTOP-U1O6GVP\Ken <kencit47@gmail.com>
2019-06-04 14:15:46 +0700

    + Add practice 1 : So xuat hien 2 lan trong mang

---
+++ src/main/java/Arr_1_SoXuatHien2Lan.java
@@ -0,0 +1,19 @@
+public class Arr_1_SoXuatHien2Lan {
+
+    public String findNumber(int[] a){
+        String result = "";
+        int[] arrInt = {0,1,2,3,4,5,6,7,8,9};
+
+        for(int i : arrInt){
+            int count = 0;
+            for(int k : a){
+                if(i == k)
+                    count++;
+            }
+            if(count > 1)
+                result += i + " ";
+        }
+
+        return  result.trim();
+    }
+}

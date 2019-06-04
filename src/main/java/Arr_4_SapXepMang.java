bd3e90eb7ab6ccf51ac36f5add892796e93b1f16
DESKTOP-U1O6GVP\Ken <kencit47@gmail.com>
2019-06-04 14:03:13 +0700

    + Done

--- src/main/java/Arr_4_SapXepMang.java
+++ src/main/java/Arr_4_SapXepMang.java
@@ -1,7 +1,8 @@
 public class Arr_4_SapXepMang {
 
-    public int sapXepMang(int[] a){
-
+    public String sapXepMang(int[] a){
+        String result = "";
+        // Dua so chan sang ben trai , so le sang ben phai
         for(int i = 0; i < a.length - 1; i++){
             if(a[i] % 2 == 0)
                 continue;
@@ -13,10 +14,31 @@ public class Arr_4_SapXepMang {
                 }
             }
         }
-        String s = "";
+        // Sap xep so chan tang dan
+        for(int i = 0; a[i] % 2 == 0; i++){
+
+            for(int k = i + 1; a[k] % 2 == 0; k++){
+                if(a[i] > a[k]){
+                    int temp = a[i];
+                    a[i] = a[k];
+                    a[k] = temp;
+                }
+            }
+        }
+        // Sap xep so le giam dan
+        for(int i = a.length - 1; a[i] % 2 != 0; i--){
+
+            for(int k = i - 1; a[k] % 2 != 0; k--){
+                if(a[i] > a[k]){
+                    int temp = a[i];
+                    a[i] = a[k];
+                    a[k] = temp;
+                }
+            }
+        }
         for(int i : a)
-            s += i;
-        System.out.println(s);
-        return 0;
+            result += i + " ";
+
+        return result.trim();
     }
 }
--- src/test/java/Arr_4_SapXepMangTest.java
+++ src/test/java/Arr_4_SapXepMangTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 public class Arr_4_SapXepMangTest {
     @Test
     public void test_SapXepMang(){
-        int[] a = {1,2,3,4,2,6,7,8,5,9,8,7};
-        assertEquals(new Arr_4_SapXepMang().sapXepMang(a),0);
+        int[] a = {2,1,3,4,2,6,7,8,5,9,8,7};
+        assertEquals(new Arr_4_SapXepMang().sapXepMang(a),"2 2 4 6 8 8 9 7 7 5 3 1");
     }
 }

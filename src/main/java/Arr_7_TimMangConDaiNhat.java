ff2489dde508d96bd8ca9abe5cf6735c6bca833d
DESKTOP-U1O6GVP\Ken <kencit47@gmail.com>
2019-06-05 07:51:49 +0700

    + Add practice 7 : Tim mang con dai nhat

---
+++ src/main/java/Arr_7_TimMangConDaiNhat.java
@@ -0,0 +1,11 @@
+public class Arr_7_TimMangConDaiNhat {
+
+    public String timMangConDaiNhat(int[] a){
+        String result = "";
+
+        for(int i : a){
+
+        }
+        return result;
+    }
+}
---
+++ src/test/java/Arr_7_TimMangConDaiNhatTest.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class Arr_7_TimMangConDaiNhatTest {
+    @Test
+    public void test_TimMangConDaiNhat(){
+
+    }
+}

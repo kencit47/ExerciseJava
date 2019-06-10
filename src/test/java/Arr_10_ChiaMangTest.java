f7fd4200c8229c32d6447f775a83d8958919a1c2
DESKTOP-U1O6GVP\Ken <kencit47@gmail.com>
2019-06-10 07:25:54 +0700

    + Add practice 10 : Chia mang

--- src/test/java/Arr_10_ChiaMangTest.java
+++ src/test/java/Arr_10_ChiaMangTest.java
@@ -6,6 +6,6 @@ public class Arr_10_ChiaMangTest {
     @Test
     public void test_ChiaMang(){
         int[] a = {1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1};
-        assertEquals(new Arr_10_ChiaMang().chiaMang(4,a)," ");
+        assertEquals(new Arr_10_ChiaMang().chiaMang(4,a),"123456 789123 456789 1");
     }
 }
